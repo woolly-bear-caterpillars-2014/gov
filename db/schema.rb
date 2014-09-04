0f4c3db79b08b565bd063164eb58f63d34ae0315
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-04 12:51:30 -0400

    tested database as pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140904162141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "fedregs", force: true do |t|
     t.string   "type_of_doc"
@@ -25,6 +23,6 @@ ActiveRecord::Schema.define(version: 0) do
     t.date     "pub_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-  
+  end
 
 end
