8279981cb6ec9f505da78a487c471d2587a98c48
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-04 11:58:19 -0400

    configured Twitter client in environment as a constant

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+CLIENT = REST::Client.new do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
