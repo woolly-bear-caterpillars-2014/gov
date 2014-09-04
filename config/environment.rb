4b2f3674ea61987f29c3579c6844e5f387192df2
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-04 12:03:14 -0400

    configured Twitter YML file

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,13 @@ Rails.application.initialize!
 
 
 #Twitter configuration
+
+twitter_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yml'))
+
+twitter_config.each do |key, value|
+	ENV[key]=value
+end
+
 CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_KEY']
   config.consumer_secret = ENV['TWITTER_SECRET']
