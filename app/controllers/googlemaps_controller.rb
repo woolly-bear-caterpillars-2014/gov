3ad0117941feb999e8d9e0d748df17d51683ff33
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-09-05 19:05:13 -0400

    added googlemaps example

--- app/controllers/googlemaps_controller.rb
+++ app/controllers/googlemaps_controller.rb
@@ -3,4 +3,6 @@ class GooglemapsController < ApplicationController
     # @articles = Article.all
     # @result_set = FederalRegister::Article.search(:conditions => {:term => "Accessibility"})
   end
+
+  
 end
