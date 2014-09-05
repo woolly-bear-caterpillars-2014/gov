e411d22a6e46d11beb842a2770c8017716295755
Eric Shemtov <shemtov.eric@gmail.com>
2014-09-05 16:59:47 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,20 @@
 # result_set.each do |doc|
 # 	Fedreg.create(type_of_doc: doc.type, title: doc.title, abstract: doc.abstract, pub_date: doc.publication_date)
 # end
-FederalRegisterHelper.search_by_date('2014-06-01')['results'].each do |result|
+LegislationsHelper.search_by_date_fedreg('2014-06-01')['results'].each do |result|
 	Legislation.create(
 	  title: result['title'],
 	  publication_date: result['publication_date'],
       abstract: result['abstract'],
       url: result['html_url'])
+end
+
+LegislationsHelper.search_by_date_govtrack('2011-01-05')['objects'].each do |object|
+	Legislation.create(
+	  title: object['title_without_number'],
+	  publication_date: object['introduced_date'],
+      abstract: object['titles'][0][2],
+      url: object['thomas_link'])
 end
 
 # keywords_to_fetch = Legistation.take(10)
