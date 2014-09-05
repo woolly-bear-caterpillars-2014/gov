4120f1dd00bc253716e4523b331525a0af593009
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-05 15:08:35 -0400

    wrote seed method for Legislation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-result_set = FederalRegister::Article.search(:conditions => {:term => "Accessibility"}).take(50)
+# result_set = FederalRegister::Article.search(:conditions => {:term => "Accessibility"}).take(50)
 
-result_set.each do |doc|
-	Fedreg.create(type_of_doc: doc.type, title: doc.title, abstract: doc.abstract, pub_date: doc.publication_date)
+# result_set.each do |doc|
+# 	Fedreg.create(type_of_doc: doc.type, title: doc.title, abstract: doc.abstract, pub_date: doc.publication_date)
+# end
+
+
+
+
+FederalRegisterHelper.search_by_date('2014-06-01')['results'].each do |result|
+	Legislation.create(
+	  title: result['title'],
+	  publication_date: result['publication_date'],
+      abstract: result['abstract'],
+      url: result['html_url'])
 end
+
+# keywords_to_fetch = Legistation.take(10)
+
+Legislation.all.each do |leg|
+	NytimesHelper.query_by_keywords(leg.title)
+end
+
+# keywords_to_fetch.each do |legislation|
+# 	k = NytimesHelper.query_by_keywords(legislation.title)
+# 	k.each do |article|
+# 		article[]
+# end
+
+#   t.string :title
+#       t.text :first_paragraph
+#       t.date :publication_date
+#       t.string :url
+#       t.string :source
+
+
+
+
+#  headline = @parsed_reply["response"]["docs"].first["headline"]["main"]
+# 			 snippet = @parsed_reply["response"]["docs"].first["snippet"]
+# 			 url = @parsed_reply["response"]["docs"].first["web_url"]
+
+
+
+
