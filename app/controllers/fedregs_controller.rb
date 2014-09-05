7e94912767b1cf06748efa9ce2dfbab500d01a0e
Chase Blokker <chaseblokker@yahoo.com>
2014-09-04 23:26:53 -0400

    added some search terms to search the fed reg database.  Played around with ajax but did not get it to work.

--- app/controllers/fedregs_controller.rb
+++ app/controllers/fedregs_controller.rb
@@ -2,4 +2,18 @@ class FedregsController < ApplicationController
 	def index
     p @result_set = FederalRegister::Article.search(:conditions => {:term => "Accessibility"})
   end
+
+  def new
+  end
+
+  def create
+    agency_name = params[:agency_name]
+    p @result_set = FederalRegister::Article.search(conditions: {term: agency_name})
+
+    if request.xhr?
+      render json: @result_set.to_json
+    else
+      render :index
+    end
+  end
 end
