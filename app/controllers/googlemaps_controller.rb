b23745f68528baaffb79b4d4667baf012f27a785
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-06 13:56:44 -0400

    added gmaps gem js

--- app/controllers/googlemaps_controller.rb
+++ app/controllers/googlemaps_controller.rb
@@ -2,7 +2,13 @@ class GooglemapsController < ApplicationController
 	def index
     # @articles = Article.all
     # @result_set = FederalRegister::Article.search(:conditions => {:term => "Accessibility"})
-  end
+	end
+
+	@users = STREAMINGCLIENT.filter('-180,-90,180,90')
+	@hash = Gmaps4rails.build_markers(@users) do |user, marker|
+  	marker.lat user.latitude
+  	marker.lng user.longitude
+end
 
   
 end
