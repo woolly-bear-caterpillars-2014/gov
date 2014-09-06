bc696b13f9a26a2cb25399ffb0097be8f4a07f28
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-09-06 15:00:26 -0400

    create congresspeople class

--- db/migrate/20140906184640_create_congress_people.rb
+++ db/migrate/20140906184640_create_congress_people.rb
@@ -1,7 +1,8 @@
 class CreateCongressPeople < ActiveRecord::Migration
   def change
     create_table :congress_people do |t|
-
+    	t.string :first_name, :last_name, :gender, :title, :party, :phone_number, :twitter_id, :youtube_id, :district, :website_url
+    	t.belongs_to :state
       t.timestamps
     end
   end
