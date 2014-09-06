e7e9b1354ff570dd2b26f236d4a2468258bf5e2b
Chase Blokker <chaseblokker@yahoo.com>
2014-09-06 19:41:37 -0400

    commented out tweet stream helper

--- app/helpers/tweet_stream_helper.rb
+++ app/helpers/tweet_stream_helper.rb
@@ -6,13 +6,13 @@ module TweetStreamHelper
 		ACCESS_TOKEN_SECRET = ENV["ACCESS_TOKEN_SECRET"]
 
 		def tw_config
-			TweetStream.configure do |config|
-				config.consumer_key = TWITTER_KEY['consumer_key']
-				config.consumer_secret    = TWITTER_SECRET['consumer_secret']
-				config.oauth_token = ACCESS_TOKEN['oauth_token']
-				config.oauth_token_secret = ACCESS_TOKEN_SECRET['oauth_token_secret']
-				config.auth_method = :oauth
-			end
+			# TweetStream.configure do |config|
+			# 	config.consumer_key = TWITTER_KEY['consumer_key']
+			# 	config.consumer_secret    = TWITTER_SECRET['consumer_secret']
+			# 	config.oauth_token = ACCESS_TOKEN['oauth_token']
+			# 	config.oauth_token_secret = ACCESS_TOKEN_SECRET['oauth_token_secret']
+			# 	config.auth_method = :oauth
+			# end
 		end
 	end
 end
