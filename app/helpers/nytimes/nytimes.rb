3a68f0cc1d55b6a46970608df78cf8aa690ea443
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-09-08 14:33:49 -0400

    fixed syntax nytimes helper

--- app/helpers/nytimes/nytimes.rb
+++ app/helpers/nytimes/nytimes.rb
@@ -4,6 +4,7 @@ require 'net/http'
 require 'json'
 require 'open-uri'
 require 'forwardable'
+
 module NYTimes
 	API_KEY = ENV['ARTICLE_SEARCH']
 	BASE_URI = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
@@ -11,20 +12,17 @@ module NYTimes
 	class NYTimes
 		attr_accessor :url, :parsed_reply
 
-			def get_url(string)
-				params = string.split(' ').join('&')
-				@url = ("#{BASE_URI}" + "q=#{params}&fq=pub_year:(2014)" + "&api-key=#{API_KEY}")
-			end
-		
-			def query_by_keywords(search)
-				get_url(search)
-				uri = URI(url)
-				reply = uri.read
-				@parsed_reply = JSON.parse reply
-			end
+		def get_url(string)
+			params = string.split(' ').join('&')
+			@url = ("#{BASE_URI}" + "q=#{params}&fq=pub_year:(2014)" + "&api-key=#{API_KEY}")
 		end
-	end
-
+	
+		def query_by_keywords(search)
+			get_url(search)
+			uri = URI(url)
+			reply = uri.read
+			@parsed_reply = JSON.parse reply
+		end
 
 		def pass_back_to_user
 			 headline = @parsed_reply["response"]["docs"].first["headline"]["main"]
