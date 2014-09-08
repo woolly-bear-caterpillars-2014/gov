3f622d575525598eb41e0b5786e322df36e05e52
Eric Shemtov <shemtov.eric@gmail.com>
2014-09-08 16:42:06 -0400

    fixed gmaps with helper

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,15 +1,19 @@
   function generateTweetMap(){
   map = void 0;
-  console.log('set map');
+
+  var styleArray = [{featureType:"water",elementType:"geometry",stylers:[{"color":"#193341"}]},{featureType:"landscape",elementType:"geometry",stylers:[{"color":"#2c5a71"}]},{featureType:"road",elementType:"geometry",stylers:[{"color":"#29768a"},{"lightness":-37}]},{featureType:"poi",elementType:"geometry",stylers:[{"color":"#406d80"}]},{featureType:"transit",elementType:"geometry",stylers:[{"color":"#406d80"}]},{elementType:"labels.text.stroke",stylers:[{"visibility":"on"},{"color":"#3e606f"},{"weight":2},{"gamma":0.84}]},{elementType:"labels.text.fill",stylers:[{"color":"#ffffff"}]},{featureType:"administrative",elementType:"geometry",stylers:[{"weight":0.6},{"color":"#1a3541"}]},{elementType:"labels.icon",stylers:[{"visibility":"off"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{"color":"#2c5a71"}]}];
 
   $(document).ready(function() {
     var mapOptions;
     mapOptions = void 0;
     mapOptions = {
-      zoom: 3,
-      center: new google.maps.LatLng(30.297018, 0.851440),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoomControl: true
+      zoom: 4,
+    center: new google.maps.LatLng(41.850033, -87.6500523),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: false,
+    disableDoubleClickZoom: false,
+    zoomControl: true,
+    styles: styleArray
     };
     return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   });
@@ -28,12 +32,21 @@
         lat = data.geo.coordinates[0];
         lng = data.geo.coordinates[1];
         text = data.text;
+        username = data.user.screen_name;
         latlng = new google.maps.LatLng(lat, lng);
         marker = new google.maps.Marker({
           position: latlng,
-          icon: 'http://maps.google.com/mapfiles/kml/pal4/icon47.png',
+          icon: 'tweet.png',
+          animation: google.maps.Animation.DROP,
+          draggable: false,
           title: text
         });
+        var infowindow = new google.maps.InfoWindow({
+            content: 'Tweet: <b>' + text + '</b><br>' + username
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+          });
         return marker.setMap(map);
         $('.tweet-sidebar').append(text);
       });
