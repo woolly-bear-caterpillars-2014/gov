581d18281fa601a90a4dce7612faf0d24c56bc13
Eric Shemtov <shemtov.eric@gmail.com>
2014-09-08 15:32:49 -0400

    broke twitter stream in to helper, cleaned up old helpers

--- app/controllers/gmaps_controller.rb
+++ app/controllers/gmaps_controller.rb
@@ -1,28 +1,7 @@
 class GmapsController < ApplicationController
-
-  # GET /gmaps
-  # GET /gmaps.json
-   require 'twitter'
   respond_to :html, :json, :js
   def index
-    run
-  end
-
-#copy and paste the above into the rails console
-#this will print the coordinates as an array of two strings like [ 'long', 'lat']
-#we need to pass these coordinates to the api and plot them
-
-def run
-    Thread.new do
-      puts "in thread"
-      STREAMINGCLIENT.filter(:locations => '-74,40,-73,41') do |tweet|
-          File.open("public/data/data.json","w") do |f|
-            f.write(tweet.to_json)
-            end
-        puts tweet.text
-        puts tweet.geo.coordinates
-      end 
-    end
+    TweetStreamHelper.run
   end
 end
 
