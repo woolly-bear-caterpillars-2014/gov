5c26c7f40eedae3514e429126196f3f94baa7c8e
Chase Blokker <chaseblokker@yahoo.com>
2014-09-09 10:39:19 -0400

    added text box to senator profile page for email

--- app/controllers/congress_people_controller.rb
+++ app/controllers/congress_people_controller.rb
@@ -1,38 +1,29 @@
 class CongressPeopleController < ApplicationController
-  before_action :set_congress_person, only: [:show, :edit, :update, :destroy]
-
   def index
     @congress_people = CongressPerson.all
   end
 
   def show
     @congress_person = CongressPerson.find(params[:id])
-    # @congress_person_articles = @congress_person.articles
   end
 
-  # GET /congress_people/new
   def new
     @congress_person = CongressPerson.new
   end
 
-  # GET /congress_people/1/edit
   def edit
   end
 
-  # POST /congress_people
-  # POST /congress_people.json
   def create
     @congress_person = CongressPerson.new(congress_person_params)
 
-    respond_to do |format|
-      if @congress_person.save
-        format.html { redirect_to @congress_person, notice: 'Congress person was successfully created.' }
-        format.json { render :show, status: :created, location: @congress_person }
-      else
-        format.html { render :new }
-        format.json { render json: @congress_person.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to congess_person_path
   end
 
+  def update
+    CongressPerson.find(params[:id])
+    send_email_to_congress_member(params[:email])
+
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'states#index'
   resources :states, only: [:index, :show]
-  resources :congress_people, only: [:index, :show]
+  resources :congress_people
 	resources :gmaps, only: [:index]
 	resources :articles, only: [:index]
 end
