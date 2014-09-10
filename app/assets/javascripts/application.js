9986f84b0900bee905603cdf691b406fcea9cb6a
Chase Blokker <chaseblokker@yahoo.com>
2014-09-10 12:08:58 -0400

    refoactored javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,74 +11,13 @@
 //= require d3
 //= require d3.layout.cloud
 //= require congress_people
-//= require sly
+//= require congress_profile
 
 $(document).ready(function() {
-  var $buttonEmail = $('button[name=email]'),
-      $email = $('congress-email'),
-
-      $buttonArticle = $('button[name=articles]'),
-      $buttonBill = $('button[name=bills]'),
-
-      $articles = $('#congress-articles'),
-      $bills = $('#congress-bills'),
-      $btn = $('.btn'),
-
-      $congressContent = $('.congress-content');
-
-
-  $buttonArticle.on('click', articleButtonToggle)
-  $buttonBill.on('click', billButtonToggle)
-  $buttonEmail.on('click', emailButtonAccordian)
-  $btn.on('click', toggleButtons);
-
-  function toggleButtons() {
-    $btn.not(this).removeClass('btnactive');
-    $(this).toggleClass('btnactive');
-  }
-
-  function articleButtonToggle() {
-    $('html,body').animate({scrollTop: $congressContent.offset().top - 60}, 'slow');
-    $bills.fadeOut('fast');
-    $articles.fadeIn('fast');
-  }
-
-  function billButtonToggle() {
-    $('html,body').animate({scrollTop: $congressContent.offset().top - 60}, 'slow');
-    $articles.fadeOut('fast');
-    $bills.fadeIn('fast');
-  }
-
-  function emailButtonAccordian() {
-    $(this).next().slideToggle('fast');
-  }
-
-  generate_sly();
-  if ($('.state-map').size() > 0) {
-    getRepubDemCount();
-  }
-
-  if ($('.gmap-area').size() > 0) {
-    generateTweetMap();
-  }
+  if ($('.state-map').size() > 0) { getRepubDemCount(); }
+  if ($('.gmap-area').size() > 0) { generateTweetMap(); }
 
   getSentimentAnalysis();
+  generateCongressProfileListeners();
 });
-
-function generate_sly() {
-  $('#frame').sly({
-    horizontal: 1,
-    itemNav: 'centered',
-    smart: 1,
-    activateOn: 'click',
-    scrollBy: 1,
-    mouseDragging: 1,
-    swingSpeed: 0.2,
-    scrollBar: $('.scrollbar'),
-    dragHandle: 1,
-    speed: 600,
-    startAt: 1,
-    keyboardNavBy: 'horizontal'
-  });
-}
 
