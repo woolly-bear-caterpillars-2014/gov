15a6de400439244559d9815ffc4dc9299d101b98
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-09-10 14:24:23 -0400

    added states creation into a service class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,64 +5,9 @@ congress_people.each do |congressperson|
 	person.update(sunlight_updates)
 end
 
-state_list = {
-	'AL' => "Alabama",
-	'AK' => "Alaska",
-	'AZ' => "Arizona",
-	'AR' => "Arkansas",
-	'CA' => "California",
-	'CO' => "Colorado",
-	'CT' => "Connecticut",
-	'DE' => "Delaware",
-	'DC' => "District Of Columbia",
-	'FL' => "Florida",
-	'GA' => "Georgia",
-	'HI' => "Hawaii",
-	'ID' => "Idaho",
-	'IL' => "Illinois",
-	'IN' => "Indiana",
-	'IA' => "Iowa",
-	'KS' => "Kansas",
-	'KY' => "Kentucky",
-	'LA' => "Louisiana",
-	'ME' => "Maine",
-	'MD' => "Maryland",
-	'MA' => "Massachusetts",
-	'MI' => "Michigan",
-	'MN' => "Minnesota",
-	'MS' => "Mississippi",
-	'MO' => "Missouri",
-	'MT' => "Montana",
-	'NE' => "Nebraska",
-	'NV' => "Nevada",
-	'NH' => "New Hampshire",
-	'NJ' => "New Jersey",
-	'NM' => "New Mexico",
-	'NY' => "New York",
-	'NC' => "North Carolina",
-	'ND' => "North Dakota",
-	'OH' => "Ohio",
-	'OK' => "Oklahoma",
-	'OR' => "Oregon",
-	'PA' => "Pennsylvania",
-	'RI' => "Rhode Island",
-	'SC' => "South Carolina",
-	'SD' => "South Dakota",
-	'TN' => "Tennessee",
-	'TX' => "Texas",
-	'UT' => "Utah",
-	'VT' => "Vermont",
-	'VA' => "Virginia",
-	'WA' => "Washington",
-	'WV' => "West Virginia",
-	'WI' => "Wisconsin",
-	'WY' => "Wyoming"
-}
-
+state_list = StatesService::STATE_LIST
 state_list.each do |state_abrev, state_full|
-	s = State.find_by(abbreviation: state_abrev)
-	s.name = state_full
-	s.save
+	State.create(abbreviation: state_abrev, name: state_full)
 end
 
 CongressPerson.all.each do |person|
