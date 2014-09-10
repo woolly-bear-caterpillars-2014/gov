3ac837c02070ac3dec065d0549449d5f342bf4ba
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-09-10 17:31:42 -0400

    pulling from upstream

--- app/services/sunlight_congress_service.rb
+++ app/services/sunlight_congress_service.rb
@@ -2,83 +2,73 @@ require 'net/http'
 require 'json'
 require 'open-uri'
 
-module SunlightCongressService
-	class << self
-		attr_accessor :url, :parsed_reply
+class SunlightCongressService
+	attr_accessor :url, :parsed_reply
 
-		BILL_URI = 'http://congress.api.sunlightfoundation.com/bills?'
-		LEGISLATORS_URI = 'http://congress.api.sunlightfoundation.com/legislators?'
-		API_KEY = ENV['SUNLIGHT_KEY']
+	BILL_URI = 'http://congress.api.sunlightfoundation.com/bills?'
+	LEGISLATORS_URI = 'http://congress.api.sunlightfoundation.com/legislators?'
+	API_KEY = ENV['SUNLIGHT_KEY']
 
-		def get_bills(sponsor_id)
-			url = ("#{BILL_URI}" + "sponsor_id=#{sponsor_id}" + "&apikey=#{API_KEY}")
-			p url
-			uri = URI(url)
-			reply = uri.read
-			parsed_reply = JSON.parse reply
-			bills = get_filtered_bills(parsed_reply)
+	def self.get_bills(sponsor_id)
+		url = ("#{BILL_URI}" + "sponsor_id=#{sponsor_id}" + "&apikey=#{API_KEY}")
+		p url
+		uri = URI(url)
+		reply = uri.read
+		parsed_reply = JSON.parse reply
+		bills = get_filtered_bills(parsed_reply)
+	end
+
+	def self.updated_legislator_info(bioguide_id)
+		url = ("#{LEGISLATORS_URI}" + "bioguide_id=#{bioguide_id}" + "&apikey=#{API_KEY}")
+		p url
+		uri = URI(url)
+		reply = uri.read
+		parsed_reply = JSON.parse reply
+		legislators = filter_legislator(parsed_reply)
+	end
+
+	private
+
+	def self.get_filtered_bills(parsed_reply)
+		bills = Array.new
+		parsed_reply["results"].each do |bill_hash|
+			bills << filter_bill(bill_hash)
 		end
+		bills
+	end
 
-		def updated_legislator_info(bioguide_id)
-			url = ("#{LEGISLATORS_URI}" + "bioguide_id=#{bioguide_id}" + "&apikey=#{API_KEY}")
-			p url
-			uri = URI(url)
-			reply = uri.read
-			parsed_reply = JSON.parse reply
-			legislators = filter_legislator(parsed_reply)
+	def self.filter_bill(bill_hash)
+		bill = Hash.new
+		bill[:number] = bill_hash["number"]
+		if bill_hash["last_version"] != nil
+			bill[:bill_id] = bill_hash["last_version"]["bill_version_id"]
+			bill[:pdf_url] = bill_hash["last_version"]["urls"]["pdf"]
+		else
+			bill[:bill_id] = "invalid"
+			bill[:pdf_url] = "invalid"
 		end
+		bill[:introduced_on] = parse_date(bill_hash["introduced_on"])
+		bill[:last_version_on] = parse_date(bill_hash["last_version_on"])
+		bill[:official_title] = bill_hash["official_title"]
+		bill[:short_title] = bill_hash["short_title"]
+		bill
+	end
 
-		private
-
-		def get_filtered_bills(parsed_reply)
-			bills = Array.new
-			parsed_reply["results"].each do |bill_hash|
-				bills << filter_bill(bill_hash)
-			end
-			bills
+	def self.filter_legislator(person_hash)
+		person = Hash.new
+		unless person_hash["results"].empty?
+			person[:bioguide_id] = person_hash["results"].first["bioguide_id"]
+			person[:oc_email] = person_hash["results"].first["oc_email"]
+			person[:office_adress] = person_hash["results"].first["first_name"]
+			person[:contact_form] = person_hash["results"].first["last_name"]
+			person[:term_start] = parse_date(person_hash["results"].first["term_start"])
+			person[:term_end] = parse_date(person_hash["results"].first["term_end"])
+			person[:birthday] = parse_date(person_hash["results"].first["birthday"])
 		end
+		person
+	end
 
-		def filter_bill(bill_hash)
-			bill = Hash.new
-			bill[:number] = bill_hash["number"]
-			if bill_hash["last_version"] != nil
-				bill[:bill_id] = bill_hash["last_version"]["bill_version_id"]
-				bill[:pdf_url] = bill_hash["last_version"]["urls"]["pdf"]
-			else
-				bill[:bill_id] = "invalid"
-				bill[:pdf_url] = "invalid"
-			end
-			bill[:introduced_on] = parse_date(bill_hash["introduced_on"])
-			bill[:last_version_on] = parse_date(bill_hash["last_version_on"])
-			bill[:official_title] = bill_hash["official_title"]
-			bill[:short_title] = bill_hash["short_title"]
-			bill
-		end
-
-		def filter_legislator(person_hash)
-			person = Hash.new
-			unless person_hash["results"].empty?
-				person[:bioguide_id] = person_hash["results"].first["bioguide_id"]
-				person[:oc_email] = person_hash["results"].first["oc_email"]
-				person[:office_adress] = person_hash["results"].first["first_name"]
-				person[:contact_form] = person_hash["results"].first["last_name"]
-				person[:term_start] = parse_date(person_hash["results"].first["term_start"])
-				person[:term_end] = parse_date(person_hash["results"].first["term_end"])
-				person[:birthday] = parse_date(person_hash["results"].first["birthday"])
-			end
-			person
-		end
-
-		def parse_date(date_str)
-			Date.strptime(date_str, "%Y-%m-%d")
-		end
+	def self.parse_date(date_str)
+		Date.strptime(date_str, "%Y-%m-%d")
 	end
 end
-
-# sponsor_id = "L000575"
-# bills = SunlightCongressHelper.get_bills(sponsor_id)
-
-# sponsor_id = CongressPerson.first.bioguide_id
-# bills = SunlightCongressHelper.get_bills(sponsor_id)
-# bills.each do |bill| puts bill[:title] end
-
--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,28 +1,26 @@
-module TwitterService
-	class << self
-		TWITTER_KEY = ENV["TWITTER_KEY"]
-		TWITTER_SECRET = ENV["TWITTER_SECRET"]
-		ACCESS_TOKEN = ENV["ACCESS_TOKEN"]
-		ACCESS_TOKEN_SECRET = ENV["ACCESS_TOKEN_SECRET"]
+class TwitterService
+	TWITTER_KEY = ENV["TWITTER_KEY"]
+	TWITTER_SECRET = ENV["TWITTER_SECRET"]
+	ACCESS_TOKEN = ENV["ACCESS_TOKEN"]
+	ACCESS_TOKEN_SECRET = ENV["ACCESS_TOKEN_SECRET"]
 
-		def last_arr
-			last_names = ['Barack','Obama']
-			CongressPerson.all.each do |c|
-				last_names << c.last_name
-			end
-			return last_names.take(299).join(',')
+	def self.last_arr
+		last_names = ['Barack','Obama']
+		CongressPerson.all.each do |c|
+			last_names << c.last_name
 		end
-
-	def run
-	    Thread.new do
-	     	STREAMINGCLIENT.filter(:track => last_arr) do |tweet|
-	      	if !tweet.geo.coordinates.nil?
-		        File.open("public/data/data.json","w") do |f|
-		          f.write(tweet.to_json)
-		        end
-          end
-	      end 
-	    end
-	  end
+		return last_names.take(299).join(',')
 	end
+
+	def self.run
+    Thread.new do
+     	STREAMINGCLIENT.filter(:track => last_arr) do |tweet|
+      	unless tweet.geo.coordinates.nil?
+	        File.open("public/data/data.json","w") do |f|
+	          f.write(tweet.to_json)
+	        end
+        end
+      end 
+    end
+  end
 end
