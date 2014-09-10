f57367ecbcc412e190703d814add049149b19c9d
Eric Shemtov <shemtov.eric@gmail.com>
2014-09-10 16:11:20 -0400

    fixed mailer

---
+++ app/models/send_message.rb
@@ -0,0 +1,15 @@
+class SendMessage < ActiveRecord::Base
+	def initialize(args={})
+		@from = args[:from]
+		@to = args[:to]
+		@subject = "From a constituent of yours"
+		@body = args[:body]
+	end
+
+	def send_mail_gem
+		mail = Mail.new(from: @from, to: @to, subject: @subject, body: @body)
+		mail.to_s
+		mail.delivery_method :sendmail
+		mail.deliver
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   root 'states#index'
-  resources :states, only: [:index, :show]
-  resources :congress_people
 
-
-
+  post 'send_messages' => 'send_messages#send_mail'
   get 'congress_people/:id/sentiment_visualization'=> 'congress_people#sentiment_visualization', as: 'data_visualization'
-
-
+	get 'congress_people/:id/articles' => 'congress_people#show_articles', as: 'show_articles'
+	get 'congress_people/:id/bills' => 'congress_people#show_bills', as: 'show_bills'
+	
+	resources :states, only: [:index, :show]
+	resources :congress_people
 	resources :gmaps, only: [:index]
 	resources :articles, only: [:index]
 
-  get 'congress_people/:id/articles' => 'congress_people#show_articles', as: 'show_articles'
-  get 'congress_people/:id/bills' => 'congress_people#show_bills', as: 'show_bills'
+  
 end
